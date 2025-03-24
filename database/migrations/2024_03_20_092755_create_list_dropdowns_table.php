18a3f9a60960a71d5afc567a7ec963e224046869
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-03-24 13:08:37 +0800

    asd

--- database/migrations/2024_03_20_092755_create_list_dropdowns_table.php
+++ database/migrations/2024_03_20_092755_create_list_dropdowns_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->tinyIncrements('id');
             $table->string('name',100);
             $table->string('classification',20);  
-            $table->string('type',20)->defualt('n/a');  
+            $table->string('type',100)->defualt('n/a');  
             $table->string('color',20)->default('n/a');  
             $table->string('others')->default('n/a');
             $table->boolean('is_active')->default(1);

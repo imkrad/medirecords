d7cd9e27498485d3a3f220dc1c2c1ddba7065a2c
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-03-25 14:39:28 +0800

    asd

--- resources/js/Pages/Dashboard/Family.vue
+++ resources/js/Pages/Dashboard/Family.vue
@@ -7,13 +7,28 @@
                         <table class="table table-bordered table-nowrap align-middle">
                             <thead class="bg-primary text-white">
                                 <tr class="fs-10">
-                                    <th class="text-center align-middle" rowspan="2">Fullname</th>
+                                    <th class="text-center align-middle" rowspan="2" style="width: 120px;">Registration</th>
+                                    <th class="text-center align-middle" rowspan="2" style="width: 180px;">Fullname</th>
+                                    <th class="text-center align-middle" rowspan="2" style="width: 150px;">Client Type</th>
+                                    <th class="text-center align-middle" rowspan="2" style="width: 150px;">Method</th>
                                     <th class="text-center" colspan="12">Follow-up Visits</th>
                                     <th class="text-center" colspan="3">Deworming Drugs Given to 20-40 yrs old</th>
                                     <th class="text-center align-middle" rowspan="2">Drop-out</th>
                                 </tr>
                                 <tr class="fs-10">
                                     <th class="text-center align-middle" v-for="(list,index) in months" v-bind:key="index">{{list.name}}</th>
+                                    <th class="text-center align-middle">Jan</th>
+                                    <th class="text-center align-middle">Feb</th>
+                                    <th class="text-center align-middle">Mar</th>
+                                    <th class="text-center align-middle">Apr</th>
+                                    <th class="text-center align-middle">May</th>
+                                    <th class="text-center align-middle">Jun</th>
+                                    <th class="text-center align-middle">Jul</th>
+                                    <th class="text-center align-middle">Aug</th>
+                                    <th class="text-center align-middle">Sep</th>
+                                    <th class="text-center align-middle">Oct</th>
+                                    <th class="text-center align-middle">Nov</th>
+                                    <th class="text-center align-middle">Dec</th>
                                     <th class="text-center align-middle">1st dose given</th>
                                     <th class="text-center align-middle">2nd dose given</th>
                                     <th class="text-center align-middle">3rd dose given</th>
